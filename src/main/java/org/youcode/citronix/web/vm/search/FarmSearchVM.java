59f525178a5d594030faa8ab3449e6d1f2b474f8
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2024-11-16 19:23:59 +0100

    add farm  vm and search vm

---
+++ src/main/java/org/youcode/citronix/web/vm/search/FarmSearchVM.java
@@ -0,0 +1,13 @@
+package org.youcode.citronix.web.vm.search;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FarmSearchVM {
+    private String name;
+    private String location;
+    private String creationDate;
+}
