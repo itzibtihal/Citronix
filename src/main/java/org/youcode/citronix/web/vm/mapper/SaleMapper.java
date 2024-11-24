f7ff2b8b778cc04772e5071f249f4b1e185f2288
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2024-11-24 14:28:48 +0100

    update mappers

--- src/main/java/org/youcode/citronix/web/vm/mapper/SaleMapper.java
+++ src/main/java/org/youcode/citronix/web/vm/mapper/SaleMapper.java
@@ -10,15 +10,10 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface SaleMapper {
 
-    /**
-     * Convert Sale entity to SaleResponseVM.
-     */
     @Mapping(target = "clientId", source = "client.id")
     @Mapping(target = "harvestId", source = "harvest.id")
     SaleResponseVM saleToSaleResponseVM(Sale sale);
 
-    /**
-     * Convert a list of Sale entities to a list of SaleResponseVMs.
-     */
+
     List<SaleResponseVM> salesToSaleResponseVMs(List<Sale> sales);
 }
