c4813f9f2ef79d4f6692285534dd9d1f59cffadf
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2024-11-24 14:29:35 +0100

    update repositories

--- src/main/java/org/youcode/citronix/repositories/HarvestRepository.java
+++ src/main/java/org/youcode/citronix/repositories/HarvestRepository.java
@@ -1,18 +1,20 @@
 package org.youcode.citronix.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.youcode.citronix.domain.Harvest;
 import org.youcode.citronix.domain.enums.Season;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface HarvestRepository extends JpaRepository<Harvest, UUID> {
 
-    Optional<Harvest> findById(UUID harvestId);
+    @Query("SELECT COUNT(h) > 0 FROM Harvest h WHERE h.season = :season AND h.id IN " +
+            "(SELECT hd.harvest.id FROM HarvestDetail hd WHERE hd.tree.field.id = :fieldId)")
+    boolean existsByFieldIdAndSeason(@Param("fieldId") UUID fieldId, @Param("season") Season season);
 
-    List<Harvest> findByFieldId(UUID fieldId);
-
-    boolean existsByFieldIdAndSeason(UUID fieldId, Season season);
+    @Query("SELECT h FROM Harvest h WHERE h.season = :season")
+    List<Harvest> findAllBySeason(@Param("season") Season season);
 }
